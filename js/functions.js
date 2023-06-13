35e3d8e233555feea1fb79151114231a244fe40a
Alexander <sneijder23@ya.ru>
2023-06-13 19:26:01 +0300

    fixed eslint

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,6 @@
-function validateStringLength(string, validLength) {
-  return string.length <= validLength;
-}
+const validateStringLength = (string, validLength) => string.length <= validLength;
 
-// console.log(validateStringLength ('проверяемая строка', 20));
+validateStringLength('проверяемая строка', 20);
 
 function validatePalindrome(string) {
   const normalizeString = string.replaceAll(' ', '').toLowerCase();
@@ -13,7 +11,7 @@ function validatePalindrome(string) {
   return normalizeString === resultString;
 }
 
-// console.log(validatePalindrome('Лёша на полке клопа нашёл '))
+validatePalindrome('Лёша на полке клопа нашёл ');
 
 function getNumbersFromString(string) {
   string = typeof string === 'number' ? string.toString() : string; // если в качестве параметра пришло число.
@@ -22,11 +20,11 @@ function getNumbersFromString(string) {
   for (let i = 0; i <= string.length - 1; i++) {
     const number = parseInt(string[i], 10);
 
-    if (Number.isNaN(number) === false) {
+    if (!Number.isNaN(number)) {
       result += number;
     }
   }
   return result.length > 0 ? Number(result) : NaN;
 }
 
-// console.log(getNumbersFromString('1 кефир, 0.5 батона'))
+getNumbersFromString('1 кефир, 0.5 батона');
