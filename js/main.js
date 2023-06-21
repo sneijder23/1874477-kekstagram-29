565c9e757f71819ac9c8bf1ef5470ad1ab1defc7
Alexander <sneijder23@ya.ru>
2023-06-21 19:36:56 +0300

    use JSDoc

--- js/main.js
+++ js/main.js
@@ -23,6 +23,12 @@ const DESCRIPTIONS = [
   'Как проходит мой отпуск. Найди меня на фото!'
 ];
 
+/**
+ * Получение случайного числа с заданого интервала
+ * @param {number} min - начало интервала
+ * @param {number} max - конец интервала
+ * @returns - Возвращает случайное целое число из интервала
+ */
 const getRandomInteger = (min, max) => {
   const lower = Math.ceil(Math.min(min, max));
   const upper = Math.floor(Math.max(min, max));
@@ -30,6 +36,14 @@ const getRandomInteger = (min, max) => {
   return Math.floor(result);
 };
 
+/**
+ *
+ * @param {number} min - начало интервала
+ * @param {number} max - конец интервала
+ * @param {array} previousValues - вспомогательный массив, для исключения уже полученных уникальных чисел
+ * @param if используется для остановки цикла, когда перебраны все числа из заданного диапозона min, max
+ * @returns - Возвращает случайное уникальное целое число из интервала
+ */
 const createRandomIdFromRangeGenerator = (min, max) => {
   const previousValues = [];
 
@@ -46,14 +60,38 @@ const createRandomIdFromRangeGenerator = (min, max) => {
   };
 };
 
+/**
+ * Функция для получения случайного элемента из переданного массива
+ * @param {array} elements - переданный массив
+ * @param {number} getRandomInteger - генерация случайного числа в интервале от 0 до elements.length
+ * @returns
+ */
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
+const COMMENT_COUNT = 30;
+
+/**
+ * Функция для создания комментариев
+ * @param {number} count - количество уникальных комментариев
+ * @param {array} comments - массив уникальных комментариев
+ * @param {number} COMMENT_COUNT - количество комментариев
+ * @returns в случае получения 1 сообщения возвращаем его
+ * @returns в случае получения 2 сообщений проверяем методом filter на уникальность сообщения и возвращаем их
+ * @param for цикл для создания уникальных комментариев
+ * @returns возвращает готовый объект комментария
+ */
 const createComments = (count) => {
   const comments = [];
 
-  const generateRandomCommentId = createRandomIdFromRangeGenerator(1, 30);
+  const generateRandomCommentId = createRandomIdFromRangeGenerator(1, COMMENT_COUNT);
 
-  const message = () => {
+  /**
+   * Функция для получения сообщения в комментарий
+   * @param {number} numberOfMessages - рандомно получаем 1 или 2 сообщения
+   * @returns в случае получения 1 сообщения возвращаем его
+    * @returns в случае получения 2 сообщений проверяем методом filter на уникальность сообщения и возвращаем их
+   */
+  const createMessage = () => {
     const numberOfMessages = getRandomInteger(1, 2);
     if (numberOfMessages === 1) {
       return getRandomArrayElement(MESSAGES);
@@ -70,7 +108,7 @@ const createComments = (count) => {
     const comment = {
       id: generateRandomCommentId(),
       avatar: `img/avatar-${generateRandomAvatarId}.svg`,
-      message: message(),
+      message: createMessage(),
       name: getRandomArrayElement(NAMES)
     };
 
@@ -80,21 +118,27 @@ const createComments = (count) => {
   return comments;
 };
 
-
+/**
+ * Функция для создания уникальных карточек фото
+ * @param {number} count - количество уникальных фото
+ * @param {number} generateRandomCardId - генерация уникального неповторяющего id для карточки
+ * @param {number} generateRandomPhotoId - генерация уникального неповторяющего id для url фото
+ * @returns возвращает массив с объектами уникальных карточек фото с описанием
+ */
 const createPhoto = (count) => {
   const photos = [];
 
-  const generateRandomDescriptionId = createRandomIdFromRangeGenerator(1, 25);
+  const generateRandomCardId = createRandomIdFromRangeGenerator(1, 25);
   const generateRandomPhotoId = createRandomIdFromRangeGenerator(1, 25);
 
   for (let i = 0; i < count; i++) {
 
     const photo = {
-      id: generateRandomDescriptionId(),
+      id: generateRandomCardId(),
       url: `photos/${generateRandomPhotoId()}.jpg`,
       desctiption: getRandomArrayElement(DESCRIPTIONS),
       likes: getRandomInteger(15, 200),
-      comments: createComments(getRandomInteger(0, 30)),
+      comments: createComments(getRandomInteger(0, COMMENT_COUNT)),
     };
 
     photos.push(photo);
